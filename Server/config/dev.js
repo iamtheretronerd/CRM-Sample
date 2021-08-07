933b27d8b49594d7597bffc5f8a4be25aa11e32f
Yaswanth L <65965757+dlyaswanth@users.noreply.github.com>
2021-08-07 23:48:02 +0530

    Update dev.js

--- Server/config/dev.js
+++ Server/config/dev.js
@@ -1,3 +1,3 @@
 module.exports={
-    MONGOURI:"mongodb+srv://admin:5fTRJy7eLRfly7J2@cluster0.mnefz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",   
+       MONGOURI:"" //your url ,   
 }
